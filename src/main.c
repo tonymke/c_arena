4886a23e58918ffe926631bb23cdbecd25e9869e
Tony Lechner <tony@tony-lechner.com>
2026-05-11 20:54:07 -0500

    Migrate to autotools

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
+#include <config.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
 {
-	if (puts("Hello world") == EOF) {
+	if (puts("Hello from " PACKAGE_STRING "!") == EOF) {
 		perror("puts");
 		exit(EXIT_FAILURE);
 	}
